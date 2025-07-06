1c9047464a930bc588d2c2030f00e30f2240617c
thethalles <thallessantana-2010@hotmail.com.br>
2025-07-06 13:22:42 -0300

    Implantation of "CategoriaService"

---
+++ src/main/java/br/com/sp/gov/fatec/itu/contacthub_backend/services/CategoriaService.java
@@ -0,0 +1,44 @@
+package br.com.sp.gov.fatec.itu.contacthub_backend.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.sp.gov.fatec.itu.contacthub_backend.entities.Categoria;
+import br.com.sp.gov.fatec.itu.contacthub_backend.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public List<Categoria> listarTodas() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria buscarPorId(Long id) {
+        return categoriaRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Categoria não encontrada com o id: " + id));
+    }
+
+    public Categoria criar(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria atualizar(Long id, Categoria categoria) {
+        Categoria existente = buscarPorId(id);
+        existente.setNome(categoria.getNome());
+        return categoriaRepository.save(existente);
+    }
+
+    public void deletar(Long id) {
+        if (!categoriaRepository.existsById(id)) {
+            throw new RuntimeException("Categoria não encontrada com o id: " + id);
+        }
+        categoriaRepository.deleteById(id);
+    }
+}
